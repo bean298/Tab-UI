d52bba64e1c82a44104c3c7b8a539b45bc5e7731
bean298 <tuanbin29082003@gmail.com>
2024-08-06 10:13:03 +0700

    Add logic JS

---
+++ main.js
@@ -0,0 +1,27 @@
+//Alias
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+//Use alias to get element
+const tabs = $$(".tab-item");
+const panels = $$(".tab-pane");
+
+const tabActive = $(".tab-item.active");
+const line = $(".tabs .line");
+
+tabs.forEach((tab, index) => {
+  const pane = panels[index];
+
+  tab.onclick = function () {
+    //Remove class last tab
+    $(".tab-item.active").classList.remove("active");
+    $(".tab-pane.active").classList.remove("active");
+
+    line.style.left = this.offsetLeft + "px";
+    line.style.width = this.offsetWidth + "px";
+
+    //Add class at current tab
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
+});
